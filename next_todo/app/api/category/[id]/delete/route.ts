c19124549d01e8099ecc280d048ca684eba17690
AsamiYamau <audmen.sachs@gmail.com>
2025-06-08 20:46:06 +0900

    エラー解消試し

--- next_todo/app/api/category/[id]/delete/route.ts
+++ next_todo/app/api/category/[id]/delete/route.ts
@@ -1,13 +1,7 @@
-// ✅ app/api/category/[id]/delete/route.ts
-
 import { NextResponse } from 'next/server';
 import { deleteCategory } from '@/app/lib/actions';
 
-export async function DELETE(
-  req: Request,
-  context: { params: { id: string } }
-) {
-  const id = context.params.id;
-  await deleteCategory(id);
+export async function DELETE(req: Request, { params }: { params: { id: string } }) {
+  await deleteCategory(params.id);
   return NextResponse.json({ ok: true });
 }
--- next_todo/app/api/checklist/[id]/delete/route.ts
+++ next_todo/app/api/checklist/[id]/delete/route.ts
@@ -1,13 +1,7 @@
-
-
 import { NextResponse } from 'next/server';
 import { deleteCheckList } from '@/app/lib/actions';
 
-export async function DELETE(
-  req: Request,
-  context: { params: { id: string } }
-) {
-  const id = context.params.id;
-  await deleteCheckList(id);
+export async function DELETE(req: Request, { params }: { params: { id: string } }) {
+  await deleteCheckList(params.id);
   return NextResponse.json({ ok: true });
 }
