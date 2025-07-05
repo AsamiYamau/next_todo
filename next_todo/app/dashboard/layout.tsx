15919809f18b3898f58962c7b554a2e61710cfb8
AsamiYamau <audmen.sachs@gmail.com>
2025-07-05 13:21:03 +0900

    ログイン調整・リダイレクト処理

--- next_todo/app/dashboard/layout.tsx
+++ next_todo/app/dashboard/layout.tsx
@@ -1,7 +1,13 @@
 import SideNav from '@/app/ui/dashboard/sidenav';
 import Header from '@/app/ui/dashboard/header';
+
+import { getServerSession } from 'next-auth';
+import { authOptions } from '@/app/lib/auth';
+import { redirect } from 'next/navigation';
  
-export default function Layout({ children }: { children: React.ReactNode }) {
+export default async function Layout({ children }: { children: React.ReactNode }) {
+    const session = await getServerSession(authOptions);
+    if (!session) redirect('/login');
   return (
     <>
     <div className="">
--- next_todo/app/dashboard/page.tsx
+++ next_todo/app/dashboard/page.tsx
@@ -1,12 +1,7 @@
 import { lusitana } from '@/app/ui/fonts';
 
-import { getServerSession } from 'next-auth';
-import { authOptions } from '@/app/lib/auth';
-import { redirect } from 'next/navigation';
-
 export default async function Page() {
-  const session = await getServerSession(authOptions);
-  if (!session) redirect('/login');
+
   return (
     <main>
       <h1 className={`${lusitana.className} mb-4 text-xl md:text-2xl`}>
